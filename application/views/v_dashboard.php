a6191167da1b950b2b2cc8d1230eba33a75627d0
nianurafisah15 <nianurafisah@gmail.com>
2022-11-29 08:23:16 +0700

    klinikci

---
+++ application/views/v_dashboard.php
@@ -0,0 +1,7 @@
+<div class="container-fluid">
+    <div class="jumbotron">
+        <h1 class="display-4">KLINIK PACAR JENO</h1>
+        <p class="lead">Dia yang memiliki kesehatan memiliki harapan dan dia yang memiliki harapan memiliki segalanya</p>
+        <hr class="my-4">
+    </div>
+</div>
